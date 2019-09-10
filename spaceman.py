86ab8a5ebf7595af6b30753740f00b441d6c6438
stanleychow <chowtech1022@gmail.com>
2019-09-09 23:50:03 -0700

    Update a few functions

--- spaceman.py
+++ spaceman.py
@@ -11,6 +11,7 @@ def load_word():
     words_list = f.readlines()
     f.close()
 
+    words_list = words_list[0].split(' ') #comment this line out if you use a words.txt file with each word on a new line
     secret_word = random.choice(words_list)
     return secret_word
 
@@ -24,7 +25,12 @@ def is_word_guessed(secret_word, letters_guessed):
         bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
     '''
     # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-
+    for letter in secret_word:
+        if letter in letters_guessed:
+            continue
+        else:
+             return False
+        return True
     pass
 
 def get_guessed_word(secret_word, letters_guessed):
@@ -38,6 +44,12 @@ def get_guessed_word(secret_word, letters_guessed):
     '''
 
     #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
+    string = ""
+    for letter in secret_word:
+        if letter in letters_guessed:
+            string += letter
+        else:
+            string += "_"
 
     pass
 
@@ -52,6 +64,8 @@ def is_guess_in_word(guess, secret_word):
         bool: True if the guess is in the secret_word, False otherwise
     '''
     #TODO: check if the letter guess is in the secret word
+    if guess in secret_word:
+        return True
 
     pass
 
@@ -67,8 +81,15 @@ def spaceman(secret_word):
 
 
     #TODO: show the player information about the game according to the project spec
+    print("Welcome to Spaceman!"")
+    print("The secret word contains :")
 
     #TODO: Ask the player to guess one letter per round and check that it is only one letter
+    guesses = 7
+    while guesses > 0:
+        print("-------")
+        print("You have ", guesses, " remaining!")
+        print()
 
     #TODO: Check if the guessed letter is in the secret or not and give the player feedback
 
